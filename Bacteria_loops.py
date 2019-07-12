54c0d0a92b3e0d0297e85270590e0cb93b9a8dbf
e-cavan <emmacavan89@gmail.com>
2019-07-12 08:43:08 +0100

    Removed function main line

--- Bacteria_loops.py
+++ Bacteria_loops.py
@@ -3,9 +3,6 @@ import numpy as np
 from scipy.integrate import odeint
 import  matplotlib as plt
 import matplotlib.pylab as pie
-import time
-
-start_time = time.time()
 
 N = 100
 M = 50
@@ -32,61 +29,60 @@ eRt = 0
 
 pars = (N, M, u1, Rm, Rg, l, p)
 
-if __name__ == "__main__":
-    def metabolic_model(pops,N, M, u1, Rm, Rg, l, p, t):
+def metabolic_model(pops,N, M, u1, Rm, Rg, l, p, t):
 
-        N = 100
-        M = 50
+    N = 100
+    M = 50
 
-        u1 = np.zeros([N,M])
-        np.fill_diagonal(u1,1)
-        Rm = sc.full([N], (0.3))
-        Rg = sc.full([M], (0.1))
-        l = np.zeros([M,M])
-        for i in range(M-1):
-            l[i,i+1] = 0.3
-        p = np.zeros(M)
-        t = sc.linspace(0,100,11)
-        x0 = np.concatenate((sc.full([N], (0.1)),sc.full([M], (1.0))))
+    u1 = np.zeros([N,M])
+    np.fill_diagonal(u1,1)
+    Rm = sc.full([N], (0.3))
+    Rg = sc.full([M], (0.1))
+    l = np.zeros([M,M])
+    for i in range(M-1):
+        l[i,i+1] = 0.3
+    p = np.zeros(M)
+    t = sc.linspace(0,100,11)
+    x0 = np.concatenate((sc.full([N], (0.1)),sc.full([M], (1.0))))
 
-        Nt = np.array([])
-        Mt = np.array([])
+    Nt = np.array([])
+    Mt = np.array([])
 
-        x = pops
+    x = pops
 
-        for i in range(int(N)):
-            #print(i)
-            m = x[i] * Rm[i] # maintenance lost
-            et = 0
-            gt = 0
-            rgt = 0
-            for j in range(int(M)):
-                g = u1[i,j] * x[i] * x[N+j] # growth
-                gt = gt + g
-                rg = u1[i,j] * x[i] * x[N+j] * Rg[j] # loss to growth
-                rgt = rgt + rg
-                for k in range(int(M)):
-                    e = u1[i,j] * x[i] * x[N+j] * l[j,k] # excretion
-                    et = et + e     
-            xt = gt - rgt - m - et
-            Nt = np.append(Nt, xt)
+    for i in range(int(N)):
+        #print(i)
+        m = x[i] * Rm[i] # maintenance lost
+        et = 0
+        gt = 0
+        rgt = 0
+        for j in range(int(M)):
+            g = u1[i,j] * x[i] * x[N+j] # growth
+            gt = gt + g
+            rg = u1[i,j] * x[i] * x[N+j] * Rg[j] # loss to growth
+            rgt = rgt + rg
+            for k in range(int(M)):
+                e = u1[i,j] * x[i] * x[N+j] * l[j,k] # excretion
+                et = et + e     
+        xt = gt - rgt - m - et
+        Nt = np.append(Nt, xt)
 
-        for a in range(int(M)):
-            #print(a)
-            xR = 0 # external resrouces
-            gRt = 0
-            eRt = 0
-            for b in range(int(N)):
-                #print(b)
-                gR = u1[b,a] * x[b] * x[N+a] # loss of resources to growth
-                gRt = gRt + gR
-                
-                for c in range(int(M)):
-                    eR = u1[b,c] * x[b] * x[N+c] * l[c,a] # gain of resources from excretion
-                    eRt = eRt + eR
-            rt = xR + eRt - gRt
-            Mt = np.append(Mt, rt) 
-        return np.array(np.concatenate((Nt, Mt)))
+    for a in range(int(M)):
+        #print(a)
+        xR = 0 # external resrouces
+        gRt = 0
+        eRt = 0
+        for b in range(int(N)):
+            #print(b)
+            gR = u1[b,a] * x[b] * x[N+a] # loss of resources to growth
+            gRt = gRt + gR
+            
+            for c in range(int(M)):
+                eR = u1[b,c] * x[b] * x[N+c] * l[c,a] # gain of resources from excretion
+                eRt = eRt + eR
+        rt = xR + eRt - gRt
+        Mt = np.append(Mt, rt) 
+    return np.array(np.concatenate((Nt, Mt)))
 
 pops = odeint(metabolic_model, y0=x0, t=t, args=pars)
 
@@ -98,5 +94,3 @@ pie.xlabel('Time')
 pie.ylabel('Population density')
 pie.title('Consumer-Resource population dynamics')
 pie.show()
-
-print("--- %s seconds ---" % (time.time() - start_time))
