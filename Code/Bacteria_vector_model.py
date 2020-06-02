4d3a2645034b8a427a89a27501eb1b1a89ba1fdb
e-cavan <emmacavan89@gmail.com>
2020-06-02 10:18:49 +0100

    Updating all files

--- Code/Bacteria_vector_model.py
+++ Code/Bacteria_vector_model.py
@@ -22,34 +22,61 @@ def size_resp(B_rm, Ma):
     return B0_rm
 
 # Arrhenius/Sharpe-Schoolfield for maintenance growth
-def temp_growth(k, T, Tref, T_pk,N, B_g, Ma, Ea, Ea_D):
-    Sharpe = (size_growth(B_g, Ma) * np.exp((-Ea/k) * ((1/T)-(1/Tref)))/(1 + (Ea/(Ea_D - Ea)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
+def temp_growth(k, T, Tref, T_pk,N, B_g, Ma, Ea_U, Ea_D):
+    Sharpe = (size_growth(B_g, Ma) * np.exp((-Ea_U/k) * ((1/T)-(1/Tref)))/(1 + (Ea_U/(Ea_D - Ea_U)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
     return Sharpe
 
 # Arrhenius/Sharpe-Schoolfield for maintenance respiration
-def temp_resp(k, T, Tref, T_pk,N, B_rm, Ma, Ea, Ea_D):
-    Sharpe = (size_resp(B_rm, Ma) * np.exp((-Ea/k) * ((1/T)-(1/Tref)))/(1 + (Ea/(Ea_D - Ea)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
+def temp_resp(k, T, Tref, T_pk,N, B_rm, Ma,  Ea_R, Ea_D):
+    Sharpe = (size_resp(B_rm, Ma) * np.exp((-Ea_R/k) * ((1/T)-(1/Tref)))/(1 + (Ea_R/(Ea_D - Ea_R)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
     return Sharpe
 
 # Parameters
-def params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D):
+def params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D, Rm_func):
     #Uptake
     u1 = np.zeros([M,M])
-    u_temp = temp_growth(k, T, Tref, T_pk, N, B_g, Ma, Ea, Ea_D) # uptake rates and maintenance are temp-dependant
+    u_temp = temp_growth(k, T, Tref, T_pk, N, B_g, Ma, Ea_U, Ea_D) # uptake rates and maintenance are temp-dependant
     np.fill_diagonal(u1,u_temp[0:M]) # fill in temp dependant uptake on diagonals
     u2 = np.zeros([M,M])
-    np.fill_diagonal(u2,u_temp[M:M*2]) # fill in temp dependant uptake on diagonals
+    u3 = np.zeros([M,M])
     if N == M:
         U = u1
+    elif M == N/2:
+        np.fill_diagonal(u2,u_temp[M:M*2]) # fill in temp dependant uptake on diagonals
+        U = np.concatenate((u1, u2), axis=0)
     else:
         np.fill_diagonal(u2,u_temp[M:M*2]) # fill in temp dependant uptake on diagonals
-        U = np.concatenate((u1, u2), axis=0)  
-    
+        np.fill_diagonal(u3,u_temp[M+1:M*3])
+        U = np.concatenate((u1, u2, u3), axis=0)
+        
     # Maintenance respiration
-    ar_rm = temp_resp(k, T, Tref,T_pk, N, B_rm, Ma, Ea, Ea_D) # find how varies with temperature (ar = arrhenius)
-    #Rm = sc.full([N], (0.3))
-    Rm = ar_rm
+    # ar_rm = temp_resp(k, T, Tref,T_pk, N, B_rm, Ma, Ea_R, Ea_D) # find how varies with temperature (ar = arrhenius)
+    # #Rm = sc.full([N], (0.3))
+    # Rm = ar_rm
+
+    # Rm = np.repeat(np.array([0.7, U[2,2] * 0.7 / U[0,0], U[4,4] * 0.7 / U[0,0],U[6,1] * 0.7 / U[0,0]*1.1, U[8,3] * 0.7 / U[0,0]*1.2]),Fg_num)  # force contraints
     
+    ### Calc  Rm as function  of U/Rm rule for competitors. Then need to back-calc Ea.
+
+    Rm_2 = np.array([])
+    if M == N:
+        Rm = temp_resp(k, T, Tref,T_pk, N, B_rm, Ma, Ea_R, Ea_D) # find how varies with temperature (ar = arrhenius)
+    elif M == N/2:
+        Rm_1 = temp_resp(k, T, Tref,T_pk, N, B_rm, Ma, Ea_R, Ea_D)[0:M] # find how varies with temperature (ar = arrhenius)
+        for g in range(0, M):
+            #print(g)
+            Rm_1x = range(0,M) # upper diag row
+            Rm_1y = range(0,M) #np.where(np.diag(U)) # upper diag column
+            Rm_2x = np.array(np.where(np.diag(U[M:N]))) + M # lower diag row
+            Rm_2y = range(0,M) # lower diag column
+            Rm_i = U[Rm_2x[0,g], Rm_2y[g]] * Rm_1[g] * Rm_func[g]/U[Rm_1x[g], Rm_1y[g]]
+            #print(Rm_i)
+            Rm_2 = np.append(Rm_2, Rm_i)
+            #print(Rm_2)
+        Rm = np.concatenate((Rm_1, Rm_2))
+ 
+
+
     # Growth respiration
     Rg = sc.full([M], (0))
     
@@ -60,7 +87,7 @@ def params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D):
     l[M-1,0] = 0.4
 
     # External resource input
-    p = np.repeat(1, M) #np.ones(M) #np.concatenate((np.array([0.4]), np.repeat(0.0, M-1)))  #
+    p = np.concatenate((np.array([1]), np.repeat(1, M-1)))  #np.repeat(1, M) #np.ones(M) #
     return U, Rm, Rg, l, p
 
 def metabolic_model(pops,t):
@@ -88,7 +115,7 @@ def metabolic_model(pops,t):
 ######## Set up parameters ###########
 
 N = 10 # Number of species
-M = 10 # Number of nutrients
+M = 5 # Number of nutrients
 K = 10 # number of species (* 100)
 Fg = 5 # number of functional groups
 Fg_num = N/Fg # number of species in each functional group
@@ -102,12 +129,13 @@ B_rm = 0.1 # B0 for respiration (maintence)
 #B_rm = (0.5 * B_g) - 0.1
 Ma = 1 # Mass
 Ea_D = np.repeat(3.5,N) # Deactivation energy
+#Rm_func = np.array([1, 1.1, 1.1, 1, 1]) # same or diff ratio between competitors, must be length M
 #temp = np.array([5, 10,15,20,25])
-t_n = 5 # number of temperatures (0-10 inclusive)
+t_n = 21 # number of temperatures (0-10 inclusive)
 t_n_output = (t_n * N) + N # number of rows in output array
 rep = math.ceil(N/M)
-ass = 3 # assembly number
-t_fin = 200
+ass = 5 # assembly number
+t_fin = 2000
 t = sc.linspace(0,t_fin-1,t_fin)
 x0 = np.concatenate((sc.full([N], (0.1)),sc.full([M], (1.0))))
 
@@ -118,77 +146,117 @@ result_array = np.empty((0,N+M))
 row = np.repeat(sc.linspace(1,ass,ass), N) 
 B_n = sc.linspace(1,N,N)
 N_n = np.tile(sc.linspace(1,M,M), rep)
-output_tmp_lrg = np.empty((0,10))
+output_tmp_lrg = np.empty((0,11))
 
-for i in range(4, t_n):
+for i in range(20, t_n):
 #for i in range(0, len(temp)):
 #for i in range(1,3):
 
     T = 273.15 + i # temperature 
     Temp = np.repeat(T - 273.15, N)
-    Ea_tmp2 = np.repeat(np.array([0.6, 0.7, 0.8, 0.9,1.0]),Fg_num) # Activation energy (same for g and rm)
+    Ea_U_tmp2 = np.repeat(np.array([0.6, 0.7, 0.8, 0.9,1.0]),Fg_num) # Activation energy (same for g and rm)
+    Ea_R_tmp2 = np.repeat(np.array([0.7, 0.8,  0.9, 1.0, 1.1]),Fg_num) # Activation energy (same for g and rm)
     #Ea_tmp2 = np.repeat(np.array([0.7, 0.7, 0.7, 0.7, 0.7]),Fg_num) # Activation energy (same for g and rm)
-    Ea = shuffle(Ea_tmp2, random_state=0)        
-
+    Rm_func = np.array([1, 1.1, 1, 0.9, 1])
+    Ea_U = shuffle(Ea_U_tmp2, random_state=0)        
+    Ea_R = shuffle(Ea_R_tmp2, random_state=0)        
+    #Ea_R=np.repeat(0.3, 10)
     # Varying B0
     #B_g = np.exp(2.5 + (-5 * Ea))
     #B_rm = 0.5 *(np.exp(2.5 + (-5 * Ea))) #(0.5 * (B_g)) #
 
-    output_tmp = np.empty((0,9))
+    output_tmp = np.empty((0,10))
     print('Temperature =',i)
 
     for j in range(ass):
-        t_fin=200
+        print('')
+        print('Assembly = ', j)
+        t_fin=2000
         t = sc.linspace(0,t_fin-1,t_fin)
-        Ea_1 = Ea # To fill output dataframe
+        Ea_1_U = Ea_U # To fill output dataframe
+        Ea_1_R = Ea_R # To fill output dataframe
         #B_g_1 = B_g # To fill output dataframe when B0 varies
         #B_rm_1 = B_rm # To fill output dataframe when B0 varies
         B_g_1 = np.repeat(B_g, N) # To fill output dataframe when B0 constant
         B_rm_1 = np.repeat(B_rm, N) # To fill output dataframe when B0 constant
 
+        print('Uptake Ea (C1 - C5)' + str(Ea_U[0:M]))
+        print('Uptake Ea (C6 - C10)' + str(Ea_U[M:N]))
+        print('Metabolic ratio between competitors' + str(Rm_func))
+        
         # Set up model
-        U = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[0] # make less than 1
-        Rm = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[1] # make less than 1
-        Rg = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[2] # l + Rg must be less than 1
-        l = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[3]
-        p = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[4]
+        U = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D, Rm_func)[0] # make less than 1
+        Rm = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D, Rm_func)[1] # make less than 1
+        Rg = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D, Rm_func)[2] # l + Rg must be less than 1
+        l = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D, Rm_func)[3]
+        p = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D, Rm_func)[4]
         l_sum = np.sum(l, axis=1)
+        
+        print('Metabolic ratio (C1 - C5)' + str(np.round(np.diag(U[5:10])/Rm[5:10], 3)))
+        print('Metabolic ratio (C6 - C10)' + str(np.round(np.diag(U)/Rm[0:5],3)))
+        #print(np.round(U, 3))
+        #print(np.round(Rm, 3))
 
         # Run model
         pops = odeint(metabolic_model, y0=x0, t=t)
+        pops = np.round(pops, 3)
+        print('Bacteria mass at equilibrium (C1 - C5)' + str(pops[len(pops)-1,0:M]))
+        print('Bacteria mass at equilibrium (C6 - C10)' + str(pops[len(pops)-1,M:N]))
+        print('Resource mass at equilibrium (S1 - S5)' + str(pops[len(pops)-1,N:N+M]))
 
-        # Steady state test
-        ss_test = np.round(abs((pops[t_fin-1,0:N]) - (pops[t_fin-50,0:N])),3) 
-        #print((ss_test))
-        while True:
-            if  np.any(ss_test > 0):
-                t = sc.linspace(0,99,100)
-                pops2 = odeint(metabolic_model, y0=pops[t_fin-1,:], t=t)
-                pops = np.append(pops, pops2, axis=0)
-                t_fin = t_fin + 100
-                ss_test = np.round(abs((pops[t_fin-1,0:N]) - (pops[t_fin-50,0:N])),3)
-            elif np.all(ss_test == 0):
-                break
-            else:
-                pops=pops
-        #print(t_fin)
+            # # Steady state test
+            # ss_test = np.round(abs((pops[t_fin-1,0:N]) - (pops[t_fin-50,0:N])),3) 
+            # #print((ss_test))
+            # while True:
+            #     if  np.any(ss_test > 0):
+            #         t = sc.linspace(0,99,100)
+            #         pops2 = odeint(metabolic_model, y0=pops[t_fin-1,:], t=t)
+            #         pops = np.append(pops, pops2, axis=0)
+            #         t_fin = t_fin + 100
+            #         ss_test = np.round(abs((pops[t_fin-1,0:N]) - (pops[t_fin-50,0:N])),3)
+            #     elif np.all(ss_test == 0):
+            #         break
+            #     else:
+            #         pops=pops
+            # #print(t_fin)
 
         # Output for analysis in R
         T_fin_B = pops[t_fin-1,0:N]
         T_fin_N = np.tile(pops[t_fin-1,N:N+M],rep)
         T_fin = sc.full([N], t_fin)
-        output_tmp = np.append(output_tmp, np.column_stack((T_fin_B, T_fin_N, Ea_1, Temp, B_n, N_n, T_fin, B_g_1, B_rm_1)),  axis=0)
+        output_tmp = np.append(output_tmp, np.column_stack((T_fin_B, T_fin_N, Ea_1_U, Ea_1_R, Temp, B_n, N_n, T_fin, B_g_1, B_rm_1)),  axis=0)
         
+        # if j == ass-1: 
+        #     break
+
         # Assembly
         rem_find = pops[t_fin-1,0:N] # final individuals numbers
         ext = np.where(rem_find<0.01) # Eas to keep
         rem_find = np.where(rem_find<0.01,0.1,rem_find) # replace extinct with 0.1
         x0 = np.concatenate((rem_find, pops[t_fin-1,N:N+M])) # new x0 with nutrients
+        
         # Varying temp sensitivity 
-        Ea_tmp = np.repeat(np.array([0.6, 0.7, 0.8, 0.9, 1.0]),N) # Create large temporary Ea for new species
-        np.random.shuffle(Ea_tmp) # randomise vector
-        Ea_tmp = Ea_tmp[0:len(ext[0])] # Cut vector so one Ea for each individual
-        Ea[ext] = Ea_tmp # Replace removed Eas with new Eas
+        Ea_tmp_U = np.repeat(np.array([0.6, 0.7, 0.8, 0.9, 1.0]),N) # Create large temporary Ea for new species
+        Ea_tmp_U = shuffle(Ea_U_tmp2) # randomise vector
+        Ea_tmp_U = Ea_tmp_U[0:len(ext[0])] # Cut vector so one Ea for each individual
+        Ea_U[ext] = Ea_tmp_U # Replace removed Eas with new Eas
+        #Ea_tmp_R = np.repeat(np.array([0.6, 0.7, 0.8, 0.9, 1.0]),N) # Create large temporary Ea for new species
+        # shuffle(Ea_R_tmp2, random_state=1) # randomise vector
+        # Ea_tmp_R = Ea_R_tmp2[0:len(ext[0])] # Cut vector so one Ea for each individual
+        # Ea_R[ext] = Ea_tmp_R # Replace removed Eas with new Eas
+
+        # Change relationship between old B and new one migrating in  
+        #print(ext)
+        Rm_ext_tmp = np.array(ext)
+        Rm_ext_tmp[Rm_ext_tmp>M] = Rm_ext_tmp[Rm_ext_tmp>M] - M # - M so find which nut it had
+        #print(Rm_ext_tmp)
+        Rm_ext = np.unique(Rm_ext_tmp) # remove duplicates, i.e. if both competitors die (wont happen)
+        Rm_func_new = np.array([0.8,0.9,1,1.1,1.2]) # new U/Rm conditions
+        Rm_func_new = shuffle(Rm_func_new) # shuffle U/Rm conditions
+        #Rm_func_tmp = Rm_func_new[0:len(Rm_ext)] # select how many new conditions between competitors needed
+        #Rm_func[Rm_ext] = Rm_func_tmp  # assign to new species
+        Rm_func[Rm_ext] = Rm_func_new[Rm_ext]
+        
         # Varying temperature sensitivity
         #B_g_tmp_2 = np.exp(2.5 + (-5 * Ea_tmp))
         #B_rm_tmp_2 = 0.5 *(np.exp(2.5 + (-5 * Ea_tmp))) #(0.5 * B_g_tmp_2)     
@@ -202,6 +270,7 @@ for i in range(4, t_n):
     output = np.column_stack((row, output_tmp))
     output_tmp_lrg = np.append(output_tmp_lrg, output,  axis=0)
 
+#print(result_array[len(result_array)-1,0:N]   )
 
 #### Save outputs ####
 #np.savetxt('Smith_params_new_temp_size_brm_ss.csv', output_tmp_lrg, delimiter=',')
@@ -235,10 +304,10 @@ plt.plot(t_plot, result_array[:,N:N+M], 'b-', label = 'Resources', linewidth=0.7
 plt.grid
 plt.ylabel('Population density')
 plt.xlabel('Time')
-plt.title('Bacteria-Nutrients population dynamics')
+plt.title('Bacteria-Resource population dynamics')
 plt.legend([Line2D([0], [0], color='green', lw=2), Line2D([0], [0], color='blue', lw=2)], ['Bacteria', 'Nutrients'])
 #plt.legend([Line2D([0], [0], color='green', lw=2)], ['Consumers'])
-plt.savefig('Figure_ein_exist_2.png')
+#plt.savefig('Figure_ein_exist_2.png')
 #plt.ylim([0, 10])
 plt.show()
 
--- Code/Vector_Testing_model.py
+++ Code/Vector_Testing_model.py
@@ -23,35 +23,40 @@ def size_resp(B_rm, Ma):
     return B0_rm
 
 # Arrhenius/Sharpe-Schoolfield for uptake
-def temp_growth(k, T, Tref, T_pk,N, B_g, Ma, Ea, Ea_D):
-    Sharpe = (size_growth(B_g, Ma) * np.exp((-Ea/k) * ((1/T)-(1/Tref))))#/(1 + (Ea/(Ea_D - Ea)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
+def temp_growth(k, T, Tref, T_pk,N, B_g, Ma, Ea_U, Ea_D):
+    Sharpe = (size_growth(B_g, Ma) * np.exp((-Ea_U/k) * ((1/T)-(1/Tref))))#/(1 + (Ea/(Ea_D - Ea)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
     return Sharpe
 
 # Arrhenius/Sharpe-Schoolfield for maintenance respiration
-def temp_resp(k, T, Tref, T_pk,N, B_rm, Ma, Ea, Ea_D):
-    Sharpe = (size_resp(B_rm, Ma) * np.exp((-Ea/k) * ((1/T)-(1/Tref))))#/(1 + (Ea/(Ea_D - Ea)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
+def temp_resp(k, T, Tref, T_pk,N, B_rm, Ma, Ea_R, Ea_D):
+    Sharpe = (size_resp(B_rm, Ma) * np.exp((-Ea_R/k) * ((1/T)-(1/Tref))))#/(1 + (Ea/(Ea_D - Ea)) * np.exp(Ea_D/k * (1/T_pk - 1/T))))
     return Sharpe
 
 # Parameters
-def params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D):
+def params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D):
     #Uptake
     u1 = np.zeros([M,M]) # M M
-    #u_temp = temp_growth(k, T, Tref, T_pk, N, B_g, Ma, Ea, Ea_D) # uptake rates and maintenance are temp-dependant
-    u_temp = np.array([10, 20])
+    #u_temp = temp_growth(k, T, Tref, T_pk, N, B_g, Ma, Ea_U, Ea_D) # uptake rates and maintenance are temp-dependant
+    u_temp = np.array([10, 20, 30])
     np.fill_diagonal(u1,u_temp) # fill in temp dependant uptake on diagonals
     #np.fill_diagonal(u1,1) # fill in temp dependant uptake on diagonals
     u2 = np.zeros([M,M])
+    u3 = np.zeros([M,M])
     if N == M:
         U = u1
+    elif M == N/2:
+        np.fill_diagonal(u2,u_temp[M:M*2]) # fill in temp dependant uptake on diagonals
+        U = np.concatenate((u1, u2), axis=0) 
     else:
         np.fill_diagonal(u2,u_temp[M:M*2]) # fill in temp dependant uptake on diagonals
-        U = np.concatenate((u1, u2), axis=0)  
+        np.fill_diagonal(u3,u_temp[M+1:M*3])
+        U = np.concatenate((u1, u2, u3), axis=0)  
     
     # Maintenance respiration
-    #ar_rm = temp_resp(k, T, Tref,T_pk, N, B_rm, Ma, Ea, Ea_D) # find how varies with temperature (ar = arrhenius)
+    ar_rm = temp_resp(k, T, Tref,T_pk, N, B_rm, Ma, Ea_R, Ea_D) # find how varies with temperature (ar = arrhenius)
     #Rm = sc.full([N], (0.5))
-    Rm = np.array([1, 2])
-    #Rm = ar_rm
+    Rm = ar_rm
+    #Rm = np.array([1, 2])
     
     # Growth respiration
     Rg = sc.full([M], (0))
@@ -67,7 +72,7 @@ def params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D):
 
     return U, Rm, Rg, l, p
 
-def metabolic_model(pops,t, U, Rm, Rg, l, p, l_sum, Ea, Ea_D, N, M, T, Tref, B_rm, B_g, Ma, k):
+def metabolic_model(pops,t, U, Rm, Rg, l, p, l_sum, Ea_U,Ea_, Ea_D, N, M, T, Tref, B_rm, B_g, Ma, k):
     x = pops
 
     xc =  x[0:N] # consumer
@@ -90,7 +95,7 @@ def metabolic_model(pops,t, U, Rm, Rg, l, p, l_sum, Ea, Ea_D, N, M, T, Tref, B_r
 
 ######## Set up parameters ###########
 
-N = 2 # Number of species
+N = 3 # Number of species
 M = 1 # Number of nutrients
 K = 2 # number of species (* 100)
 k = 0.0000862
@@ -103,62 +108,88 @@ B_rm = 0.1 #np.concatenate([np.repeat(0.3,5), np.repeat(0.1,5)]) #0.1 # B0 for r
 #B_rm = (0.5 * B_g) - 0.1
 Ma = 1 # Mass
 T = 273.15+20
-Ea = np.concatenate([np.repeat(0.6,N/2), np.repeat(1.0,N/2)])
+#Ea_U = np.array([0.6, 0.8, 1.0])
+#Ea_R = np.array([0.7,  0.9, 1.1]) # must be different ratio to change from Ea_U
+Ea_U = np.concatenate([np.repeat(0.6,N/2), np.repeat(1.0,N/2)])
+Ea_R = np.concatenate([np.repeat(0.6,N/2), np.repeat(1.0,N/2)]) # must be different ratio to change from Ea_U
 Ea_D = np.repeat(3.5,N) # Deactivation energy
-t_fin = 1000000
+t_fin = 1000
 t = sc.linspace(0,t_fin-1,t_fin)
 
 
 ##### Intergrate system forward #####
 result_array = np.empty((0,N+M)) 
 x0 = np.concatenate((sc.full([N], (0.1)),sc.full([M], (1.0))))
-#x0 = np.array([(0.11135157),(0.29801513),(0.33)])
+#x0 = np.array([(1.5),(0.4),(0.3)])
 
-# Set up model
-U = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[0]
-Rm = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[1] 
-Rg = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[2] # l + Rg must be less than 1
-l = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[3]
-p = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea, Ea_D)[4]
-l_sum = np.sum(l, axis=1)
+# plot change in ratio U/Rm vs time to equil
+ratio = np.delete(sc.linspace(0.01,2,1000), np.array(sc.linspace(490, 510, 20)))
+time_eq = np.array([])
 
-# Run model
-pars = (U, Rm, Rg, l, p, l_sum, Ea, Ea_D, N, M, T, Tref, B_rm, B_g, Ma, k)
-pops = odeint(metabolic_model, y0=x0, t=t, args = (pars))
-print(pops[t_fin-1,:])
+for i in range(len(ratio)): 
+    # Set up model
+    U = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D)[0]
+    #Rm = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R,Ea_D)[1]
+    #  
+    if M == N/2:
+        Rm = np.array([1,2*ratio[i]])
+    else:
+        Rm = np.array([1,2*ratio[i], 3*ratio[i]])
 
-#### Plot output ####
-t_plot = sc.linspace(0,len(result_array),len(result_array))
-plt.plot(t, pops[:,0:N], 'g-', label = 'Consumers', linewidth=0.7)
-plt.plot(t, pops[:,N:N+M], 'b-', label = 'Resources', linewidth=0.7)
+    Rg = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D)[2] # l + Rg must be less than 1
+    l = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D)[3]
+    p = params(N, M, T, k, Tref, T_pk, B_g, B_rm,Ma, Ea_U, Ea_R, Ea_D)[4]
+    l_sum = np.sum(l, axis=1)
+
+    # Run model
+    pars = (U, Rm, Rg, l, p, l_sum, Ea_U, Ea_R, Ea_D, N, M, T, Tref, B_rm, B_g, Ma, k)
+    pops = odeint(metabolic_model, y0=x0, t=t, args = (pars))
+    pops = np.round(pops, 3)
+    #print(pops[t_fin-1,:])
+    #print(len(np.unique(pops, axis=0)))
+
+#### outputs for time vs u/rm ratio plot
+    #time_eq = np.append(time_eq, len(np.unique(pops, axis=0))) # how long to equil
+    time_eq = np.append(time_eq, np.where(pops[:,:] == 0)[0][0]) # when a B goes to 0
+plt.plot(ratio, time_eq, 'ro')
 plt.grid
-plt.ylabel('Population density')
-plt.xlabel('Time')
-plt.title('Bacteria-Substrate population dynamics')
-plt.legend([Line2D([0], [0], color='green', lw=2), Line2D([0], [0], color='blue', lw=2)], ['Bacteria', 'Substrate'])
-plt.savefig('Figure_ein_exist.png')
+plt.xlabel('Ratio (U1/Rm1 : U2/Rm2)')
+plt.ylabel('Timesteps to extinction of one bacteria')
+#plt.scatter([ratio], [time_eq], color='blue')
 plt.show()
+
+### Plot output ####
+# t_plot = sc.linspace(0,len(result_array),len(result_array))
+# plt.plot(t, pops[:,0:N], 'g-', label = 'Consumers', linewidth=0.7)
+# plt.plot(t, pops[:,N:N+M], 'b-', label = 'Resources', linewidth=0.7)
+# plt.grid
+# plt.ylabel('Population density')
+# plt.xlabel('Time')
+# plt.title('Bacteria-Substrate population dynamics')
+# plt.legend([Line2D([0], [0], color='green', lw=2), Line2D([0], [0], color='blue', lw=2)], ['Bacteria', 'Substrate'])
+# plt.savefig('Figure_ein_exist.png')
+# plt.show()
 
 
 # ---------------------------------------------------
 
 ### Findiong equil solutions using sympy 
 
-from sympy import *
-import scipy as sc
-u1, u2, S, Rg, Rm1, Rm2,a, C1, C2,rho, t = var("u1, u2, S, Rg, Rm1, Rm2,a, C1, C2, rho,t",real = True)
+# from sympy import *
+# import scipy as sc
+# u1, u2, S, Rg, Rm1, Rm2,a, C1, C2,rho, t = var("u1, u2, S, Rg, Rm1, Rm2,a, C1, C2, rho,t",real = True)
 
-dC1_dt = C1 * ((u1 * S * (1-Rg-a)) - Rm1)
-dC2_dt = C2 * ((u2 * S * (1-Rg-a)) - Rm2)
-dS_dt = rho - ((u1 * S * C1) - (u1 * S * C1 * a)) - ((u2 * S * C2) - (u2 * S * C2 * a))
-dC1_dt, dC2_dt, dS_dt
-C1_eqlb = Eq(dC1_dt, 0)
-C2_eqlb = Eq(dC2_dt, 0)
-S_eqlb = Eq(dS_dt, 0)
-C1_eqlb,C2_eqlb, S_eqlb
-C1_eqlb_sol = solve(C1_eqlb, S)
-C2_eqlb_sol = solve(C2_eqlb, S)
-S_eqlb_sol = solve(S_eqlb, C1)
+# dC1_dt = C1 * ((u1 * S * (1-Rg-a)) - Rm1)
+# dC2_dt = C2 * ((u2 * S * (1-Rg-a)) - Rm2)
+# dS_dt = rho - ((u1 * S * C1) - (u1 * S * C1 * a)) - ((u2 * S * C2) - (u2 * S * C2 * a))
+# dC1_dt, dC2_dt, dS_dt
+# C1_eqlb = Eq(dC1_dt, 0)
+# C2_eqlb = Eq(dC2_dt, 0)
+# S_eqlb = Eq(dS_dt, 0)
+# C1_eqlb,C2_eqlb, S_eqlb
+# C1_eqlb_sol = solve(C1_eqlb, S)
+# C2_eqlb_sol = solve(C2_eqlb, S)
+# S_eqlb_sol = solve(S_eqlb, C1)
 
 ### Phase plane analysis (2 species, 1 resource)
 
